41fd604391a929b208792817cb0bcba7590fdb38
ArbajAr7 <arbajrmacr786@gmail.com>
2024-07-24 13:35:48 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [clicked, setClicked] = useState(0);
+  
+  function incrementCounter() {
+    setClicked(clicked+1);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>Button clicked {clicked} times</p>
+      <button onClick={incrementCounter}>Click me</button>
+      {/* Arbaj Ansari
+        Do not remove the main div */}
     </div>
   )
 }
